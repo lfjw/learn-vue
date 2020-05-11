93b1dc6252ff21303594e9cb3dd3919b1a59d33a
jw <web_Jiwei@163.com>
2020-05-11 13:56:38 +0800

    更新内容

--- p.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-git config --global user.email "web_Jiwei@163.com"
-git add .
-git commit -m '更新内容'
-git pull
-git push
-git config --global user.email "jiwei@jiweideMacBook-Pro.local"
